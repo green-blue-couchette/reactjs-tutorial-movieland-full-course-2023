6faccb31528218b2f2e56c040e3a6db125523a04
Oliver-10.10 <antaloliver@yahoo.com>
2023-10-23 11:59:17 +0200

    Ready to create movie web app (upto 44:05 in the tutorial)

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
-import {useState} from 'react';
-import './App.css';
-import { useEffect } from 'react';
-
-/**
- * Follows the tutorial "React JS Full Course 2023 | Build an App and Master React in 1 Hour"
- * 1:11:43
- * by JavaScript Mastery on YouTube
- * - Oliver
- * 
- * Started Sunday, October 22, 2023
- */
+import React from 'react';
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    alert("Counter changed to " + counter + ".");
-  }, [counter]);
-
-  // Without a dependency array, useEffect runs every time a button is pressed
-  // With an empty dependency array [], useEffect runs only the first time a component is mounted.
-  // With a dependency array that contains something, like [counter], useEffect will run every time the prop in question is changed.
-  
-  return (
-    <div className="App">
-      <button onClick={() => {setCounter((prevCounter) => (prevCounter - 1))}}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => {setCounter((prevCounter) => (prevCounter + 1))}}>+</button>
-    </div>
-  );
+    return(
+        <h1>App</h1>
+    );
 }
 
 export default App;
