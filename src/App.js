7c130b3520f9b7f96cbd9a4b0d5bd48411cd3002
Oliver-10.10 <antaloliver@yahoo.com>
2023-10-23 11:51:40 +0200

    Implemented useEffect functionality (upto 40:45 in the tutorial)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {useState} from 'react';
 import './App.css';
+import { useEffect } from 'react';
 
 /**
  * Follows the tutorial "React JS Full Course 2023 | Build an App and Master React in 1 Hour"
@@ -13,11 +14,19 @@ import './App.css';
 const App = () => {
   const [counter, setCounter] = useState(0);
 
+  useEffect(() => {
+    alert("Counter changed to " + counter + ".");
+  }, [counter]);
+
+  // Without a dependency array, useEffect runs every time a button is pressed
+  // With an empty dependency array [], useEffect runs only the first time a component is mounted.
+  // With a dependency array that contains something, like [counter], useEffect will run every time the prop in question is changed.
+  
   return (
     <div className="App">
-      <button onClick={() => {setCounter((kitty) => (kitty - 1))}}>-</button>
+      <button onClick={() => {setCounter((prevCounter) => (prevCounter - 1))}}>-</button>
       <h1>{counter}</h1>
-      <button onClick={() => {setCounter((kitty) => (kitty + 1))}}>+</button>
+      <button onClick={() => {setCounter((prevCounter) => (prevCounter + 1))}}>+</button>
     </div>
   );
 }
