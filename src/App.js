fc428246ff5a5f96c976507e380f4df1f84af030
Oliver-10.10 <antaloliver@yahoo.com>
2023-10-25 09:24:18 +0200

    Prepared code structure for front page (App.js) (upto 55:17 in the tutorial)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import './App.css';
+import SearchIcon from './search.svg';
+
+import * as credentials from './credentials.js';
+
+const API_URL = "http://www.omdbapi.com?apikey=" + credentials.API_KEY;
 
 const App = () => {
+
+    async function searchMovies(title) {
+        const requestString = `${API_URL}&s=${title}`;
+        const response = await fetch(requestString);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+    
+    // Search for a movie whenever the app is mounted
+    useEffect(() => {
+        searchMovies('Revolution');
+    }, []);
+
     return(
-        <h1>App</h1>
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input
+                    placeholder="Search for movies!"
+                    value="Jesus Revolution"
+                    onChange={() => {}}
+                />
+
+                <img
+                src={SearchIcon}
+                alt="search"
+                onClick={() => {}}
+                />
+            </div>
+
+            <div className="container">
+
+            </div>
+            
+        </div>
     );
 }
 
