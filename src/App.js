bb626fc01c8e94b849dbd6a7954c1eabb9ce0b86
Oliver-10.10 <antaloliver@yahoo.com>
2023-10-25 12:16:53 +0200

    Finishing touches - added documentation comment to App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
+/* This web app is my implementation of the MovieLand application from this tutorial:
+"React JS Full Course 2023 | Build an App and Master React in 1 Hour"
+1:11:43
+JavaScriptMastery, YouTube
+https://www.youtube.com/watch?v=b9eMGE7QtTk
+
+Completed on Wednesday, October 25, 2023
+(This was, of course, not an exhaustive tutorial.)
+
+My steps can be followed by looking at the commit history.
+I added some of my own improvements to the search experience (searching by pressing enter, clearing the search box when a search is made, a message that shows what search term was used) -- see the "Finishing touches" commits in the commit history.
+
+- Oliver
+*/
+
 import React, {useEffect, useState, useRef} from 'react';
 import './App.css';
 import SearchIcon from './search.svg';
